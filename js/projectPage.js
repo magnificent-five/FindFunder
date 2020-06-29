f120a13ad033ba8a0de7ebe905720814544a176f
SondosMatahen <Sondosmatahen@gmail.com>
2020-06-29 17:31:17 +0300

    editprojectpage

--- js/projectPage.js
+++ js/projectPage.js
@@ -25,3 +25,55 @@ var newTitle = document.getElementById("title");
  
  var newStory = document.getElementById("story");
  newStory.textContent =visted.story;
+
+
+
+
+
+// function to get back the wallet from storage
+var myWallet;
+function getWallet (){
+    myWallet=JSON.parse(localStorage.getItem('wallet')) 
+    }
+ 
+// Create an event listener so that when the donate link is clicked, the donateFunction method is invoked.
+var donate = document.getElementById('form');
+donate.addEventListener('submit', donateFunction);
+
+
+
+function donateFunction(event) {
+// Prevent the page from reloading
+// update the amount value
+// save wallate in local storage
+// update the wallet in HTMl
+    getWallet();
+    event.preventDefault();
+    myWallet.amount -= Number(event.target.donateValue.value);
+    var donateAmount=Number(event.target.donateValue.value);
+    localStorage.setItem('wallet', JSON.stringify(myWallet));
+    var newProjectDonate= new ProjectDonate (visted , donateAmount)
+    localStorage.setItem('projectDonateArray', JSON.stringify(projectDonateArray));
+    // draw();
+
+}
+
+
+
+// creat new arry
+var projectDonateArray=[];
+//check if there is project array in storage 
+if (localStorage.getItem('projectDonateArray')){
+    projectDonateArray= JSON.parse(localStorage.getItem('projectDonateArray'));
+}
+
+
+//Project Donate Constructor 
+function ProjectDonate(obj, donateAmount){
+    this.id=obj.id;
+    this.name=obj.title
+    thid.amount=donateAmount;
+    projectDonateArray.push(this);
+}
+
+
