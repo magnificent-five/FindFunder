c6b2c6389cd51558c70139119dac0d2f3d487c71
waleedfarraj <waleed.farraj95@gmail.com>
2020-06-29 16:35:56 +0300

    all linked missing calcualtion js and some css

--- js/app.js
+++ js/app.js
@@ -3,33 +3,36 @@
 
 var allProjects = [];
 
+// this adds an invent listner to each option in the catagory  nav
+// and it takes you to that page and renders all projects related to 
+// that catagory .
 var dorpDownOptionMedical= document.getElementById('Medical');
 dorpDownOptionMedical.addEventListener('click', redirectToMedical);
 function redirectToMedical(event){
-//    event.preventDefault();
+
     var loadInfo=[];
     loadInfo = JSON.parse(localStorage.getItem('medicalArr'));
     console.log(loadInfo);
    localStorage.setItem('loadInfo', JSON.stringify(loadInfo));
-   //window.location="catagory-page.html"
+  
 }
 
 var dorpDownOptionEduction= document.getElementById('Education');
 dorpDownOptionEduction.addEventListener('click', redirectToEducation);
 function redirectToEducation(event){
-    // event.preventDefault();
+    
     var loadInfo=[];
    loadInfo = JSON.parse(localStorage.getItem('EducationArr'));
    localStorage.setItem('loadInfo', JSON.stringify(loadInfo));
-//    window.location="catagory-page.html"
+
 }
 
 var dorpDownOptionnonProfit= document.getElementById('non-profit');
 dorpDownOptionnonProfit.addEventListener('click', redirectToNonProfit);
 function redirectToNonProfit(event){
-    // event.preventDefault();
+    
     var loadInfo=[];
    loadInfo = JSON.parse(localStorage.getItem('non-profitArr'));
    localStorage.setItem('loadInfo', JSON.stringify(loadInfo));
-//    window.location="catagory-page.html"
+
 }
