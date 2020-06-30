403a6fe4b45603302af9ccf03a5ffa0a57c7b685
mohammad samara <mohammad.ab.samara@gmail.com>
2020-06-30 10:51:45 +0300

    module

--- js/project-page.js
+++ js/project-page.js
@@ -43,7 +43,30 @@ function donateFunction(event) {
   event.preventDefault();
   //check if you have a wallet
   if (!localStorage.getItem("wallet")) {
-    alert("You haven't add a wallet yet");
+       // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// open the modal 
+  modal.style.display = "block";
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+    //alert("You haven't add a wallet yet");
     return;
   }
   myWallet = JSON.parse(localStorage.getItem("wallet"));
@@ -51,7 +74,30 @@ function donateFunction(event) {
 
   //check if you have enough money in the wallet
   if (donateAmount > myWallet.amount) {
-    alert("You don't have enough money");
+       // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// open the modal 
+  modal.style.display = "block";
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+    //alert("You don't have enough money");
     return;
   }
   myWallet.amount -= donateAmount;
