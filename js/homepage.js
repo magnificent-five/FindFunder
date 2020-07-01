a63d24954230c65314e0ea5d162568acb3361d34
Abdallah Safi <mr.abdallahsafi@gmail.com>
2020-07-02 01:07:23 +0300

    add if statment in home page to check if the allprojects exist before adding the cards

--- js/homepage.js
+++ js/homepage.js
@@ -6,31 +6,34 @@ console.log(allprojects);
 
 var perviewsection = document.getElementById("section2");
 
-for (var i = 0; i < 3; i++) {
-  var div = document.createElement("div");
-  div.id = i;
-  div.setAttribute('class' , 'styleCards')
-  var link = document.createElement("a");
-  link.addEventListener("click", redirectToProjectPage);
-  link.setAttribute("href", "project-page.html");
-  link.setAttribute("id", allprojects[i].id);
-  
-  var img = document.createElement("img");
-  img.setAttribute("src", allprojects[i].image);
-  img.setAttribute("class", "imageStyle");
-  link.appendChild(img);
-  var article = document.createElement("article");
-  var h4 = document.createElement("h4");
-  h4.textContent = allprojects[i].title;
-  article.appendChild(h4);
-  var p = document.createElement("p");
-  p.setAttribute("class", "card-info");
-  p.innerHTML = `<i class="far fa-money-bill-alt"></i> <span id="raised">$${allprojects[i].raised}</span> Raised of <i class="fas fa-bullseye"></i> <span id="goal">$${allprojects[i].goal}</span>`;
-  article.appendChild(p);
-  link.appendChild(article);
-  div.appendChild(link);
-  perviewsection.appendChild(div);
+if(allprojects){
+  for (var i = 0; i < 3; i++) {
+    var div = document.createElement("div");
+    div.id = i;
+    div.setAttribute('class' , 'styleCards')
+    var link = document.createElement("a");
+    link.addEventListener("click", redirectToProjectPage);
+    link.setAttribute("href", "project-page.html");
+    link.setAttribute("id", allprojects[i].id);
+    
+    var img = document.createElement("img");
+    img.setAttribute("src", allprojects[i].image);
+    img.setAttribute("class", "imageStyle");
+    link.appendChild(img);
+    var article = document.createElement("article");
+    var h4 = document.createElement("h4");
+    h4.textContent = allprojects[i].title;
+    article.appendChild(h4);
+    var p = document.createElement("p");
+    p.setAttribute("class", "card-info");
+    p.innerHTML = `<i class="far fa-money-bill-alt"></i> <span id="raised">$${allprojects[i].raised}</span> Raised of <i class="fas fa-bullseye"></i> <span id="goal">$${allprojects[i].goal}</span>`;
+    article.appendChild(p);
+    link.appendChild(article);
+    div.appendChild(link);
+    perviewsection.appendChild(div);
+  }
 }
+
 
 function redirectToProjectPage(event) {
   var visted = allprojects.find(checkExistance, this.id);
