c3066639dbad300614b338f30cac24865d63ba3b
mohammad samara <mohammad.ab.samara@gmail.com>
2020-07-02 09:49:46 +0300

    latest update

--- js/creatWallet.js
+++ js/creatWallet.js
@@ -46,6 +46,34 @@ function inputname(event) {
     var CreatWalletnew = new CreatWallet(name);
     console.log(name);
     saveMyWallet();
+    {
+               // Get the modal
+    var modal = document.getElementById("myModal2");
+
+    // Get the button that opens the modal
+   // var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // open the modal
+    modal.style.display = "block";
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+      modal.style.display = "none";
+    };
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    };
+    //alert("You haven't add a wallet yet");
+
+    return;
+    }
 
 }
 
